ce9015ee7e1b92b361aeab1b739f4e4ffe3e02f9
stacdera <sophia.tacderas97@yahoo.com>
2018-07-13 12:09:15 -0700

    Input data for matrix saved in matrices and matrices_data in default_index.js, specific matrix displays now when clicked from sidebar

--- web2py/applications/MAT3D/static/js/default_index2.js
+++ web2py/applications/MAT3D/static/js/default_index2.js
@@ -32,7 +32,8 @@ var app = function() {
             },
             function (data) {
                 $.web2py.enableElement($("#add_matrix_submit"));
-                self.vue.matrices.unshift(data.matrix);
+                //self.vue.matrices.unshift(data.matrix);
+                self.vue.matrices.push(data.matrix);
                 enumerate(self.vue.matrices);
                 self.vue.populate_matrix_name = self.vue.form_name;
                 self.vue.populate_cols = self.vue.form_col;
@@ -40,8 +41,8 @@ var app = function() {
                 self.vue.form_name = "";
                 self.vue.form_row = "";
                 self.vue.form_col = "";
-                self.vue.form_matrix_id = self.vue.form_matrix_id + 1;
-                self.vue.is_adding_matrix = !self.vue.is_adding_matrix
+                //self.vue.form_matrix_id = self.vue.form_matrix_id + 1;
+                self.vue.is_adding_matrix = !self.vue.is_adding_matrix;
                 self.vue.is_populating_matrix = true;
             });
     };
@@ -57,19 +58,7 @@ var app = function() {
             });
     }
 
-    /*
-    self.get_user_matrix = function (matrix_id) {
-        $.post(get_matrix_url,
-            {
-                matrix_id: matrix_id
-            },
-            function (data) {
-                self.vue.self_page = data.self_page;
-                self.vue.matrices = data.matrices;
-                enumerate(self.vue.matrices);
-        })
-    };
-    */
+
 
      self.delete_matrix = function(matrix_idx) {
          /*Delete Main Matrix */
@@ -82,11 +71,42 @@ var app = function() {
 
 
      self.get_this_matrix = function (matrix_idx) {
-
+         //Retrieve name of Matrix
+         self.vue.populate_matrix_name = self.vue.matrices[matrix_idx].name;
+         //Retrieve the clicked matrix's data
+         self.vue.one_matrix = self.vue.matrices_data[matrix_idx];
+         //To ensure the html codes goes to logic that only displays the matrix data
+         self.vue.is_populating_matrix = false;
     };
 
-     self.add_data_matrix = function () {
-        /* Write data matrix data elements to --> matrices_data */
+    self.add_data_matrix = function () {
+
+        var entered_table = document.getElementById("data_matrix")
+        var individual_row = [];
+        var individual_matrix = [];
+
+        // Loop through all rows and columns of the table
+        // and retrieve content of each cell.
+        for ( var i = 0; row = entered_table.rows[i]; i++ ) {
+            row = entered_table.rows[i];
+            for ( var j = 0; col = row.cells[j]; j++ ) {
+                //create individual array rows from HTML
+                    individual_row.push(col.firstChild.nodeValue);
+            }
+            //Add the individual array row to the individual matrix
+            individual_matrix.push(individual_row);
+            individual_row = []
+        }
+        // For debugging, to be displayed on html
+        self.vue.one_matrix = individual_matrix;
+        // Store matrix created from html into matrices_data []
+        self.vue.matrices_data.push(individual_matrix);
+        //Reset is_populating_matrix to false to prevent entry
+        self.vue.is_populating_matrix = false;
+
+
+        //Popup Alert message
+        alert("Matrix Data Saved");
 
     };
 
@@ -98,11 +118,11 @@ var app = function() {
             is_adding_matrix: false,
             matrices: [],
             matrices_data: [],
+            one_matrix: [],
             form_name: null,
             form_parsertext: null,
             form_row: null,
             form_col: null,
-            form_matrix_id: 0,
             return_message: null,
             returnmessage: null,
             self_page: false,
